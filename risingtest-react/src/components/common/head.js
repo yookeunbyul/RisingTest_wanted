e77da2cd7410cd569726447dd1295a096194c7cd
yookeunbyul <91243651+yookeunbyul@users.noreply.github.com>
2022-07-19 22:38:12 +0900

    nav 수정

--- risingtest-react/src/components/common/head.js
+++ risingtest-react/src/components/common/head.js
@@ -14,7 +14,8 @@ const Head = () => {
     };
     return(
         <Outline>
-                <div className="left">
+            <nav className="nav">
+                <div>
                     <button className="btn-menu side-margin">
                         <img className="menu" alt="" src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Ficon-menu.png&w=undefined&q=75" />
                     </button>
@@ -22,29 +23,29 @@ const Head = () => {
                     <Logo width={74.38} height={17.33}/>
                     </span>
                 </div>
-                <ul className="center">
-                    <li className="menu-list">채용</li>
-                    <li className="menu-list">이벤트</li>
-                    <li className="menu-list">직군별 연봉</li>
-                    <li className="menu-list">이력서</li>
-                    <li className="menu-list">커뮤니티</li>
-                    <li className="menu-list">프리랜서</li>
-                    <li className="menu-list">AI 합격예측</li>
+                <ul>
+                    <li className="menu-list"><a>채용</a></li>
+                    <li className="menu-list"><a>이벤트</a></li>
+                    <li className="menu-list"><a>직군별 연봉</a></li>
+                    <li className="menu-list"><a>이력서</a></li>
+                    <li className="menu-list"><a>커뮤니티</a></li>
+                    <li className="menu-list"><a>프리랜서</a></li>
+                    <li className="menu-list"><a>AI 합격예측</a></li>
                 </ul>
-                <ul className="right">
-                        <li>
-                            <button className="btn-menu"><Search /></button>
+                <aside className="aside">
+                    <ul>
+                        <li className="right-menu">
+                            <button className="search-btn"><Search /></button>
                         </li>
-                        <li>
-                            <button className="btn-menu" onClick={openMoRedux}>회원가입/로그인</button>
+                        <li className="right-menu">
+                            <button className="sign-btn" onClick={openMoRedux}>회원가입/로그인</button>
                         </li>
-                        <li>
-                            <Line></Line>
-                        </li>
-                        <li>
-                            <button className="btn-company">기업 서비스</button>
+                        <li className="right-menu division">
+                            <a className="a-dashboard">기업 서비스</a>
                         </li>
                     </ul>
+                </aside>
+            </nav>
         </Outline>
     );
 }
@@ -55,10 +56,18 @@ const Outline = styled.nav`
     border-bottom: 1px solid #dddddd;
     background-color: #fff;
 
-    display: flex;
-    flex-direction: row;
-
     padding: 0px 305px;
+
+    position: relative;
+    /* max-width: 1060px; */
+
+    .nav{
+        display: flex;
+        align-items: center;
+        /* justify-content: space-between; */
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
 
     .menu{
         width: 17px;
@@ -75,31 +84,89 @@ const Outline = styled.nav`
     }
 
     .menu-list{
-        padding: 15.5px 8px;
-        border: 1px solid blue;
-        margin: 0px 10.2px;
+        /* padding: 15.5px 0px 0px 0px; */
+        /* border-bottom: 1px solid blue; */
+        /* margin: 0px 10.2px; */
+        text-align: center;
+        height: inherit;
+        display: inline-block;
     }
 
-    .center{
-        /* border: 1px solid #222; */
-        display: flex;
-        flex-direction: row;
+    .menu-list:hover::after{
+        display: block;
+        content: "";
+        width: 100%;
+        height: 2px;
+        background-color: #dddddd;
+        /* margin: auto 10px; */
+    }
+
+    .menu-list > a{
+        position: relative;
+        vertical-align: middle;
         font-size: 14px;
+        line-height: 20px;
+        font-weight: 500;
+        padding: 15px;
+        display: inline-block;
     }
 
-    .right{
-        /* border: 1px solid #222; */
-        padding-top: 13px;
-        display: flex;
-        flex-direction: row;
+    .right-menu{
+        position: relative;
+        display: inline-block;
+        height: 100%;
+        vertical-align: middle;
+    }
+
+    .right-menu.division{
+        display: inline-flex;
+    }
+
+    .right-menu.division::before{
+        display: block;
+        content: "";
+        width: 1px;
+        height: 10px;
+        background-color: #e1e2e3;
+        margin: auto 10px;
+    }
+
+    .a-dashboard{
+        font-size: 13px;
+        color: #666;
+        line-height: 30px;
+        height: 30px;
+        border: 1px solid #e1e2e3;
+        border-radius: 15px;
+        padding: 10px 10px;
+        margin-left: 15px;
+        font-weight: 400;
+    }
+
+    .aside{
+        margin-left: 21px;
+    }
+
+    .aside > ul > li >*{
+        height: 100%;
         font-size: 14px;
-
-        margin-left: 12px;
+        /* color: #333; */
+        /* font-weight: 600; */
+        line-height: 1;
+        padding: 0 10px;
     }
 
-    .align{
-        display: flex;
-        flex-direction: row;
+    .sign-btn{
+        margin-right: 6px;
+        line-height: 1.4;
+        border: 0;
+        background: none;
+        font-weight: 600;
+    }
+
+    .search-btn{
+        border: 0;
+        background: none;
     }
 
     .btn-menu{
@@ -111,19 +178,6 @@ const Outline = styled.nav`
         margin-right: 10px;
     }
 
-    .btn-company{
-        border: 1px solid #dddddd;
-        border-radius: 30px;
-        background-color: #fff;
-        padding: 1px 8px;
-        color: #717171;
-    }
-`;
-
-const Line = styled.div`
-    border-left : 1.5px solid #f1f1f1;
-    height : 12px;
-    margin: 5px 10px 0px 10px;
 `;
 
 export default Head;
